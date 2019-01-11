eec4efec9361b53a3f2ce7c2182544b4171d4e41
nianit <niclasandersson88@gmail.com>
2019-01-11 10:06:24 +0100

    Create Connect-ExchangeOnline.ps1

---
+++ Exchange Online/Connect-ExchangeOnline.ps1
@@ -0,0 +1,6 @@
+﻿function Connect-Exo {
+    $Credentials = Get-Credential
+    $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/?proxymethod=rps -Credential $Credentials -Authentication Basic -AllowRedirection
+    Import-PSSession $Session -AllowClobber
+}
+Connect-Exo
