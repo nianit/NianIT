dc1a5161c1ed7bc9e94e93ef86b7bfc7bf3e469b
nianit <niclasandersson88@gmail.com>
2019-01-11 10:32:43 +0100

    Removed an unsuade variable.

--- Scripting functions/Write-Log.ps1
+++ Scripting functions/Write-Log.ps1
@@ -11,7 +11,6 @@ Function Write-Log
          $TimeZoneBias = Get-WmiObject -Query "Select Bias from Win32_TimeZone"
          $Date= Get-Date -Format "HH:mm:ss.fff"
          $Date2= Get-Date -Format "MM-dd-yyyy"
-         $type=1
          
          "<![LOG[$Message]LOG]!><time=$([char]34)$date$($TimeZoneBias.bias)$([char]34) date=$([char]34)$date2$([char]34) component=$([char]34)$component$([char]34) context=$([char]34)$([char]34) type=$([char]34)$severity$([char]34) thread=$([char]34)$([char]34) file=$([char]34)$([char]34)>"| Out-File -FilePath $Path -Append -NoClobber -Encoding default
 }
